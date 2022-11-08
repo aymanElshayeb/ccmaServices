e224f05a277c18fd7d59353cfa78a2c93e6e4342
nabil mokhtar <nabilmokhtar15@gmail.com>
2022-11-08 05:00:33 +0200

    modify sendMail with request object instead of parameter

--- src/main/java/com/infenion/ccmalogic/services/MailService.java
+++ src/main/java/com/infenion/ccmalogic/services/MailService.java
@@ -1,4 +1,5 @@
 package com.infenion.ccmalogic.services;
+import com.infenion.ccmamodel.model.Request;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -21,20 +22,21 @@ public class MailService {
 
 
 
-    public String sendMail(String address ,String requesterName,String ProjectName, long requesterId,long projectId,long requestId,String accessPermission,String systemName) throws MessagingException {
+
+    public String sendMail(String address , Request request) throws MessagingException {
         Context context = new Context();
-        context.setVariable("projectId", projectId);
-        context.setVariable("requestId", requestId);
-        context.setVariable("requesterId", requesterId);
-        context.setVariable("requesterName", requesterName);
-        context.setVariable("ProjectName", ProjectName);
-        context.setVariable("accessPermission", accessPermission);
-        context.setVariable("systemName", systemName);
+        context.setVariable("projectId", request.getProject().getId());
+        context.setVariable("requestId", request.getId());
+        context.setVariable("requesterId", request.getRequester().getId());
+        context.setVariable("requesterName", request.getRequester().getUserName());
+        context.setVariable("ProjectName", request.getProject().getName());
+        context.setVariable("accessPermission", request.getSystemAccess().getAccessPermission().toString());
+        context.setVariable("systemName",  request.getSystemAccess().getSystemName().toString());
 
         String process = templateEngine.process("request", context);
         javax.mail.internet.MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-        helper.setSubject("access request for project : "+ProjectName+" from "+requesterName);
+        helper.setSubject("Access request for project : "+request.getProject().getName()+" from "+request.getRequester().getUserName());
 
         helper.setText(process, true);
         helper.setTo(address);
