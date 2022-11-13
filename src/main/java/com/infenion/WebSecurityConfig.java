64be4e67de6781eafc35a101095196be557237ee
nabil mokhtar <nabilmokhtar15@gmail.com>
2022-11-13 12:50:51 +0200

    allow /requester/register from basic auth

--- src/main/java/com/infenion/WebSecurityConfig.java
+++ src/main/java/com/infenion/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/requester/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(requesterDetailService)
