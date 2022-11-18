1d310c8a839d52d56be60ad76c2d5caafed7372f
nabil mokhtar <nabilmokhtar15@gmail.com>
2022-11-18 19:30:42 +0200

    return entity instead of void and add manager name

--- src/main/java/com/infenion/ccmarest/rest/RequestActionRest.java
+++ src/main/java/com/infenion/ccmarest/rest/RequestActionRest.java
@@ -1,8 +1,10 @@
 package com.infenion.ccmarest.rest;
 
+import com.atlassian.httpclient.api.HttpStatus;
 import com.infenion.ccmamodel.model.Request;
 import com.infenion.ccmalogic.services.RequestActionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
@@ -34,15 +36,16 @@ public class RequestActionRest {
     @GetMapping("/returnToRequesterFormMail/{request}")
 
     //todo :: return response entity
-    public void returnToRequester(@PathVariable  String request) throws MessagingException {
+    public ResponseEntity<String> returnToRequester(@PathVariable  String request) throws MessagingException {
         requestActionService.returnToRequesterFromMail(Long.parseLong(request));
+        return ResponseEntity.ok("request returned");
     }
 
 
     @GetMapping("/executeFromMail/{request}")
-    public void executeFromMail(@PathVariable  String request) {
-
-         requestActionService.executeFromMail(Long.parseLong(request));
+    public ResponseEntity<String> executeFromMail(@PathVariable  String request) {
+        requestActionService.executeFromMail(Long.parseLong(request));
+        return ResponseEntity.ok("request approved");
     }
 
 }
